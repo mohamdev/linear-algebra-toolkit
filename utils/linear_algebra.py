73670a381a3ca9e23e4932dfd428594e83a887e8
Adjel Mohamed <40664995+mohamdev@users.noreply.github.com>
2025-03-27 11:13:02 +0100

    Update linear_algebra.py

--- utils/linear_algebra.py
+++ utils/linear_algebra.py
@@ -86,28 +86,3 @@ def make_homogeneous_rep_matrix(R, t):
     P[:3, 3] = t.reshape(3)
     P[3,3] = 1
     return P
-
-def butterworth_filter(data, cutoff_frequency, order=5, sampling_frequency=60):
-    nyquist = 0.5 * sampling_frequency
-    normal_cutoff = cutoff_frequency / nyquist
-    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
-    filtered_data = signal.filtfilt(b, a, data, axis=0)
-    return filtered_data
-
-def low_pass_filter_data(data,nbutter=5):
-    '''This function filters and elaborates data used in the identification process. 
-    It is based on a return of experience  of Prof Maxime Gautier (LS2N, Nantes, France)'''
-    
-    b, a = signal.butter(nbutter, 0.01*5 / 2, "low")
-   
-    #data = signal.medfilt(data, 3)
-    data= signal.filtfilt(
-            b, a, data, axis=0, padtype="odd", padlen=3 * (max(len(b), len(a)) - 1) )
-    
-    
-    # suppress end segments of samples due to the border effect
-    # nbord = 5 * nbutter
-    # data = np.delete(data, np.s_[0:nbord], axis=0)
-    # data = np.delete(data, np.s_[(data.shape[0] - nbord): data.shape[0]], axis=0)
-     
-    return data
