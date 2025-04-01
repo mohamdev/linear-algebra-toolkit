3426f5fd102f5a868fd905f56295c5c42a1d4145
root <kchalabi@laas.fr>
2025-04-01 09:39:25 +0000

    adapt calibration code

--- apps/mocap_2cam.py
+++ apps/mocap_2cam.py
@@ -3,24 +3,6 @@ from utils.rigid_bodies_algorithms import *
 from utils.read_write_utils import *
 import pandas as pd
 
-def get_mks_array_from_dict(mks_dict):
-    """
-    Converts a dictionary of markers with 3D coordinates into a 2D numpy array.
-
-    Parameters:
-    mks_dict: dict
-        Dictionary with marker names as keys and 3D coordinates (arrays) as values.
-
-    Returns:
-    mks_array: ndarray
-        2D array where each row corresponds to the 3D coordinates of a marker.
-    """
-    n_mks = len(mks_dict)  # Number of markers
-    mks_array = np.zeros((n_mks, 3))  # Initialize a 2D array for 3D coordinates
-    for i, (key, coords) in enumerate(mks_dict.items()):
-        mks_array[i] = coords  # Assign the 3D coordinates to the array
-    return mks_array
-
 mks_names = ['B']
 mks_features_names = []
 for m in mks_names:
@@ -32,27 +14,17 @@ for p in position_names:
     position = position + [f"{p}_x",f"{p}_y",f"{p}_z"]
 
 
-no_test = 4
+no_test = "rs"
+
 mocap_df = pd.read_csv(f"/root/workspace/ros_ws/src/rt-cosmik/output/test{no_test}/barycenter_raw.csv", usecols=mks_features_names).values
 postion_aruco = pd.read_csv(f"/root/workspace/ros_ws/src/rt-cosmik/output/test{no_test}/pose_aruco.csv", usecols=position).values
-# mocap_df = pd.read_csv("/root/workspace/ros_ws/src/rt-cosmik/output/aruco_pose_cam1.csv", usecols=position).values
-# postion_aruco = pd.read_csv("/root/workspace/ros_ws/src/rt-cosmik/output/aruco_pose_cam2.csv", usecols=position).values
-# n_imgs = 280
-# mocap_df = mocap_df [:n_imgs]
-# postion_aruco = postion_aruco [:n_imgs]
+
 mocap_df = np.array(mocap_df)
 postion_aruco = np.array(postion_aruco)
 
-# print(mocap_df)
-# mocap_mks_dict, _ = read_mks_data(mocap_df)
-# lstm_mks_dict, _ = read_mks_data(postion_aruco)
-
-# mks_mocap = get_mks_array_from_dict(mocap_mks_dict[0])
-# mks_lstm = get_mks_array_from_dict(lstm_mks_dict[0])
-
 
 print("-- Soder --")
-R, d, rms = soder(mocap_df, postion_aruco) #mocap vers cam
+R, d, rms = soder(postion_aruco,mocap_df) #mocap vers cam
 print("R:\n", R)
 print("d:\n", d)
 print("rms:\n", rms)
@@ -60,7 +32,7 @@ print("rms:\n", rms)
 save_transformation(f"/root/workspace/ros_ws/src/rt-cosmik/output/test{no_test}/soder.txt", R, d, 1.0, rms)
 
 print("-- Challis --")
-R, d, s, rms = challis(mocap_df, postion_aruco)
+R, d, s, rms = challis(postion_aruco,mocap_df)
 print("R:\n", R)
 print("d:\n", d)
 print("scale factor:\n", s)
